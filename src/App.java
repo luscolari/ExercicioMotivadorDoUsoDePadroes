7395ed1380d37688befc2ea9144a5259dbc97757
Lu Scolari <141656103+luscolari@users.noreply.github.com>
2024-08-14 22:00:08 +0000

    Adição de Diesel em tipos de combustível e criação do carro utilitário
     Untracked files:
    	.devcontainer/

--- src/App.java
+++ src/App.java
@@ -6,11 +6,16 @@ public class App {
         //Criação de carro esportivo que utiliza gasolina, consome 6 km/it e tem capacidade 45 litros.
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
 
+        //Criação de carro UTILIÁRIO que utiliza diesel, consome 5 km/it e tem capacidade 70 litros.
+        Carro utilitario = new Carro("Utilitário", TipoCombustivel.DIESEL, 5, 70);
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
 
         //Adição do carro esportivo em tipos de veículos
         System.out.println(esportivo);
+
+        //Adição do carro esportivo em tipos de veículos
+        System.out.print(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
